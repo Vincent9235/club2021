4fd171415dae597f53193bacc250a1144206176f
Vincent9235 <vincentf.laurens@orange.fr>
2021-03-19 11:25:52 +0100

    project init

---
+++ club2021/index.php
@@ -0,0 +1,17 @@
+<?php
+require('inc/header.php');
+?>
+
+<body>
+
+    <h1><strong>Accueil Maison des Ligues</strong></h1>
+    <h3>Vous possédez déjà un compte?</h3>
+    <a href="connexion.php">Se connecter</a>
+    <br>
+    <h4>Vous ne possédez pas encore de compte?</h4>
+    <a href="inscription.php">S'inscire</a>
+    <h5>Si vous êtes un responsable veuillez cliquez sur le lien ci-dessous:</h5>
+    <a href="mailto:vincentf.laurens@free.fr">Demande d'accès en tant que responsable</a>
+</body>
+
+</html>
