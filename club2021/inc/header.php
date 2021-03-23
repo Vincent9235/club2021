cc7168983378e468093efd5e4e86bfb742fb4f8f
Vincent9235 <80886118+Vincent9235@users.noreply.github.com>
2021-03-23 20:19:15 +0100

    Add files via upload

--- club2021/inc/header.php
+++ club2021/inc/header.php
@@ -10,11 +10,10 @@ require('db.php'); //Appel de la DB
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="author" content="Vincent Laurens">
+        <meta name="description" content="Club 2021">
         <title>Se connecter</title>
-        <link type="text/css" rel="stylesheet" href="style01.css" media="screen" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-
+        <link type="text/css" rel="stylesheet" href="css/formulaire.css" media="screen" />
     </head>
 <?php endif ?>
 
@@ -26,6 +25,8 @@ require('db.php'); //Appel de la DB
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="author" content="Vincent Laurens">
+        <meta name="description" content="Club 2021">
         <title>S'inscrire</title>
         <link type="text/css" rel="stylesheet" href="style01.css" media="screen" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
@@ -42,13 +43,16 @@ require('db.php'); //Appel de la DB
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="author" content="Vincent Laurens">
+            <meta name="description" content="Club 2021">
             <title>Accueil</title>
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         </head>
     <?php endif ?>
 
-    <!-------Condition ACTUALITE.PHP----->
-    <?php if ($_SERVER['PHP_SELF'] == "/club2021/article/actualite.php") : ?>
+    <!-------Condition ACTUALITE.PHP && ARTICLE.PHP----->
+
+    <?php if ($_SERVER['PHP_SELF'] == "/club2021/article/actualite.php" or $_SERVER['PHP_SELF'] == "/club2021/article/article.php") : ?>
         <!DOCTYPE html><!-- Doctype HTML 5-->
         <html lang="fr">
 
@@ -56,29 +60,38 @@ require('db.php'); //Appel de la DB
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="author" content="Vincent Laurens">
+            <meta name="description" content="Club 2021">
             <title>Actualités</title>
             <link type="text/css" rel="stylesheet" href="style01.css" media="screen" />
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-
+            <!---CSS Icons--->
+            <link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
         </head>
-    <?php endif ?>
-
-    <!-------Condition ARTICLE.PHP----->
-    <?php if ($_SERVER['PHP_SELF'] == "/club2021/article/article.php") : ?>
-        <!DOCTYPE html>
-        <html>
-
-        <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <title>Accueil</title>
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-
-        </head>
+        <header>
+            <div style="text-align:right">
+                <i class="fa fa-user" aria-hidden="true" style="color: #ee5f2a;"></i>
+                <?php $message = $_SESSION['auth']->user_prenom . ' ' . $_SESSION['auth']->user_nom . ' ' . '[' . $_SESSION['auth']->role_nom . ']';
+                echo ($message);
+                ?>
+                <br>
+                <?php if (isset($_SESSION['auth'])) : ?>
+                    <a href="../logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i>Se déconnecter</a>
+                <?php endif ?>
+                </br>
+            </div>
+            <?php if (isset($_SESSION['auth'])) : ?>
+                <a href="actualite.php"><i class="fa fa-home " aria-hidden="true"></i>Actualités</a>
+                <a href="../profil.php"><i class="fa fa-user" aria-hidden="true"></i>Mon profil</a>
+            <?php endif ?>
+            <?php if (isset($_SESSION['auth']) && $_SESSION['auth']->role_role == 'admin') : ?>
+                <a href="admin/page_admin.php"><i class="fa fa-cog" aria-hidden="true"></i>Administration</a>
+            <?php endif ?>
+        </header>
     <?php endif ?>
 
     <!-------Condition REDACTION.PHP----->
+
     <?php if ($_SERVER['PHP_SELF'] == "/club2021/article/redaction.php") : ?>
         <!DOCTYPE html>
         <html>
@@ -87,13 +100,40 @@ require('db.php'); //Appel de la DB
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="author" content="Vincent Laurens">
+            <meta name="description" content="Club 2021">
             <title>Rédaction / Edition</title>
+            <link rel="stylesheet" href="../css/formulaire.css">
             <link rel="stylesheet" href="../css/wbbtheme.css">
+            <!---CSS Icons--->
+            <link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
             <script src="../js/jquery.wysibb.min.js"></script>
             <script src="../js/fr.js"></script>
         </head>
+        <header>
+            <div style="text-align:right">
+                <i class="fa fa-user" aria-hidden="true" style="color: #ee5f2a;"></i>
+                <?php $message = $_SESSION['auth']->user_prenom . ' ' . $_SESSION['auth']->user_nom . ' ' . '[' . $_SESSION['auth']->role_nom . ']';
+                echo ($message);
+                ?>
+                <br>
+                <?php if (isset($_SESSION['auth'])) : ?>
+                    <a href="../logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i>Se déconnecter</a>
+                <?php endif ?>
+                </br>
+            </div>
+            <?php if (isset($_SESSION['auth'])) : ?>
+                <a href="actualite.php"><i class="fa fa-home " aria-hidden="true"></i>Actualités</a>
+                <a href="../profil.php"><i class="fa fa-user" aria-hidden="true"></i>Mon profil</a>
+            <?php endif ?>
+            <?php if (isset($_SESSION['auth']) && $_SESSION['auth']->role_role == 'admin') : ?>
+                <a href="admin/page_admin.php"><i class="fa fa-cog" aria-hidden="true"></i>Administration</a>
+            <?php endif ?>
+        </header>
     <?php endif ?>
+
+
 
     <!-------Condition PROFIL.PHP----->
     <?php if ($_SERVER['PHP_SELF'] == "/club2021/profil.php") : ?>
@@ -104,12 +144,38 @@ require('db.php'); //Appel de la DB
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="author" content="Vincent Laurens">
+            <meta name="description" content="Club 2021">
             <title>Mon profil</title>
-            <link type="text/css" rel="stylesheet" href="style01.css" media="screen" />
+            <link type="text/css" rel="stylesheet" href="css/formulaire.css" media="screen" />
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-
+            <!---CSS Icons--->
+            <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
         </head>
+        <header>
+            <div style="text-align:right">
+                <i class="fa fa-user" aria-hidden="true" style="color: #ee5f2a;"></i>
+                <?php $message = $_SESSION['auth']->user_prenom . ' ' . $_SESSION['auth']->user_nom . ' ' . '[' . $_SESSION['auth']->role_nom . ']';
+                echo ($message);
+                ?>
+                <br>
+                <?php if (isset($_SESSION['auth'])) : ?>
+                    <a href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i>Se déconnecter</a>
+                <?php endif ?>
+                </br>
+            </div>
+            <?php if (isset($_SESSION['auth'])) : ?>
+                <a href="article/actualite.php"><i class="fa fa-home " aria-hidden="true"></i>Actualités</a>
+                <a href="profil.php"><i class="fa fa-user" aria-hidden="true"></i>Mon profil</a>
+            <?php endif ?>
+            <?php if (isset($_SESSION['auth']) && $_SESSION['auth']->role_role == 'admin') : ?>
+                <a href="admin/page_admin.php"><i class="fa fa-cog" aria-hidden="true"></i>Administration</a>
+            <?php endif ?>
+        </header>
     <?php endif ?>
+
+
+
     <!-------Condition modif_profil.PHP----->
     <?php if ($_SERVER['PHP_SELF'] == "/club2021/modif_profil.php") : ?>
         <!DOCTYPE html><!-- Doctype HTML 5-->
@@ -119,9 +185,32 @@ require('db.php'); //Appel de la DB
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="author" content="Vincent Laurens">
+            <meta name="description" content="Club 2021">
             <title>Modification du profil</title>
-            <link type="text/css" rel="stylesheet" href="style01.css" media="screen" />
+            <link type="text/css" rel="stylesheet" href="css/formulaire.css" media="screen" />
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-
+            <!---CSS Icons--->
+            <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
         </head>
+        <header>
+            <div style="text-align:right">
+                <i class="fa fa-user" aria-hidden="true" style="color: #ee5f2a;"></i>
+                <?php $message = $_SESSION['auth']->user_prenom . ' ' . $_SESSION['auth']->user_nom . ' ' . '[' . $_SESSION['auth']->role_nom . ']';
+                echo ($message);
+                ?>
+                <br>
+                <?php if (isset($_SESSION['auth'])) : ?>
+                    <a href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i>Se déconnecter</a>
+                <?php endif ?>
+                </br>
+            </div>
+            <?php if (isset($_SESSION['auth'])) : ?>
+                <a href="article/actualite.php"><i class="fa fa-home " aria-hidden="true"></i>Actualités</a>
+                <a href="profil.php"><i class="fa fa-user" aria-hidden="true"></i>Mon profil</a>
+            <?php endif ?>
+            <?php if (isset($_SESSION['auth']) && $_SESSION['auth']->role_role == 'admin') : ?>
+                <a href="admin/page_admin.php"><i class="fa fa-cog" aria-hidden="true"></i>Administration</a>
+            <?php endif ?>
+        </header>
     <?php endif ?>
